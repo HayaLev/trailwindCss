5eff6ee3622ee189eca866e0bdf7395fa9ba5a92
Haya L <haya.lev08@gmail.com>
2024-11-20 15:32:55 +0200

    changement de parametrage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
+  content: ["./src/**/*.{html,js}"], // Définit où Tailwind doit chercher les classes utilisées
   theme: {
     extend: {
+      // Utilisé pour ajouter des personnalisations au thème par défaut
+      colors: {
+        green1: "hsl(148, 38%, 91%)",
+        green2: "hsl(169, 82%, 27%)",
+        green3: "hsl(169, 83%, 14%)",
+      },
       fontFamily: {
-        karla: ["karla", "sans-serif"],
+        karla: ["Karla", "sans-serif"], // Assurez-vous que "Karla" est correctement orthographié avec une majuscule
       },
     },
   },
-  content: ["./src/**/*.{html,js}"],
-  plugins: [],
+  plugins: [], // Utilisé pour ajouter des plugins optionnels
 };
